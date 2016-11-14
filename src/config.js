9ebe3a348f8f618aedb4af53c8fe1371e6504c08
Garrett Vargas <gsdriver@live.com>
2016-11-14 08:57:15 -0800

    Add option to run DynamoDB locally

--- src/config.js
+++ src/config.js
@@ -3,4 +3,7 @@ var config = {};
 // Local host settings
 config.token = "Your Yelp token goes here";
 
+// Run with a local version of DynamoDB
+config.runDBLocal = true;
+
 module.exports = config;
