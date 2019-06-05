b7ea3240f5c524b1c3a9e2aa08802ea0b128caed
Garrett Vargas <gvargas@SEA.CORP.EXPECN.COM>
2019-06-05 14:59:00 -0700

    Update welcome prompt

--- lambda/custom/intents/Launch.js
+++ lambda/custom/intents/Launch.js
@@ -12,13 +12,20 @@ module.exports = {
   },
   handle: function(handlerInput) {
     const attributes = handlerInput.attributesManager.getSessionAttributes();
+    const event = handlerInput.requestEnvelope;
 
     attributes.state = undefined;
     attributes.lastSearch = undefined;
     attributes.lastResponse = undefined;
 
+    const welcomeFormat = (event.context && event.context.System &&
+        event.context.System.device &&
+        event.context.System.device.supportedInterfaces &&
+        event.context.System.device.supportedInterfaces.Geolocation)
+        ? 'LAUNCH_WELCOME_GEO' : 'LAUNCH_WELCOME';
+
     return handlerInput.jrb
-      .speak(ri('LAUNCH_WELCOME', {
+      .speak(ri(welcomeFormat, {
         Qualifier: ri('LAUNCH_QUALIFIER'),
         Cuisine: ri('LAUNCH_CUISINE'),
         Cities: ri('LAUNCH_CITIES'),
